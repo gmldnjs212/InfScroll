f82a3c181ee4bb41e4d11d103a25e7f21b302018
heee1 <p1xell@kakao.com>
2025-02-28 17:06:09 +0900

    Feat: useQuery를 사용하여 데이터 불러오기 완료

--- React_InfiniteQueries/src/App.tsx
+++ React_InfiniteQueries/src/App.tsx
@@ -1,7 +1,24 @@
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
+
+const API_URL =
+  'https://api.thedogapi.com/v1/images/search?size=small&format=json&has_breeds=true&order=ASC&page=0&limit=10';
+
+const fetchData = async () => {
+  const response = await axios.get(API_URL);
+  return response.data;
+};
+
 const App = () => {
+  const { isLoading, data, isError, error } = useQuery({
+    queryKey: ['getData'],
+    queryFn: fetchData,
+  });
   return (
     <>
-      <h1>App Component</h1>
+      {isLoading && <p> loading.. </p>}
+      {isError && <p>Error : {error.message}</p>}
+      {data && <pre>{JSON.stringify(data, null, 2)}</pre>}
     </>
   );
 };
